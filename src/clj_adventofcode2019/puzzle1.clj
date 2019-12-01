d4e5f73800c53711a8ee913d198ec7e612e17570
etiago <tiago@espinha.co.uk>
2019-12-01 22:01:41 +0000

    Cleaning up

--- src/clj_adventofcode2019/puzzle1.clj
+++ src/clj_adventofcode2019/puzzle1.clj
@@ -1,40 +1,50 @@
 (ns clj-adventofcode2019.puzzle1
   (:gen-class))
 
+;; Consciously eagerly loading file into memory.
+;; Lazy sequence from a file makes the code slightly more ugly.
+(def puzzle-input
+  (->> (slurp "resources/puzzle1.txt")
+       (clojure.string/split-lines)
+       (map #(Double/parseDouble %))))
+
 (defn- fuel-requirements-for-mass
   [mass]
-  (as-> mass n
-    (/ n 3)
-    (Math/floor n)
-    (- n 2)))
+  (-> mass
+    (/ 3)
+    (Math/floor)
+    (- 2)))
 
 (defn- calculate-total-fuel
   [total-acc last-fuel]
   (if (<= last-fuel 0)
     total-acc
-    (recur (+ total-acc last-fuel) (fuel-requirements-for-mass last-fuel))))
+    (recur
+     (+ total-acc last-fuel)
+     (fuel-requirements-for-mass last-fuel))))
+
+(defn- total-fuel-requirements-for-mass
+  [mass]
+  (->> mass
+       (fuel-requirements-for-mass)
+       (calculate-total-fuel 0)))
+
+(defn- map-reduce-fuel-requirements-for-mass
+  [input]
+  (->> input
+      (map fuel-requirements-for-mass)
+      (reduce +)))
+
+(defn- map-reduce-total-fuel-requirements-for-mass
+  [input]
+  (->> input
+      (map total-fuel-requirements-for-mass)
+      (reduce +)))
 
 (defn run-pt1
   []
-  (with-open [rdr (clojure.java.io/reader "resources/puzzle1.txt")]
-    (reduce
-     +
-     0
-     (map
-      #(-> %
-           (Double/parseDouble)
-           (fuel-requirements-for-mass))
-      (line-seq rdr)))))
+  (map-reduce-fuel-requirements-for-mass puzzle-input))
 
 (defn run-pt2
   []
-  (with-open [rdr (clojure.java.io/reader "resources/puzzle1.txt")]
-    (reduce
-     +
-     0
-     (map
-      #(->> %
-            (Double/parseDouble)
-            (fuel-requirements-for-mass)
-            (calculate-total-fuel 0))
-      (line-seq rdr)))))
+  (map-reduce-total-fuel-requirements-for-mass puzzle-input))
